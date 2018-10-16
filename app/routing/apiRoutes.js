497c6f1699ba4543844ae3bcf6ae1cfa3753bde7
Blake Mitchell <38139493+BlakeMitchell829@users.noreply.github.com>
2018-10-15 19:36:47 -0500

    Create apiRoutes.js

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,45 @@
+const friends = require("../data/friends");
+module.exports = (app) => {
+
+  app.get("/api/friends", (req, res) => {
+    res.json(friends);
+  });
+  app.post("/api/friends", (req, res) => {
+   
+    const bestMatch = {
+      name: "",
+      photo: "",
+      friendDifference: Infinity
+    };
+
+    const userData = req.body;
+    const userScores = userData.scores;
+    const totalDifference;
+
+    for (let i = 0; i < friends.length; i++) {
+      const currentFriend = friends[i];
+      totalDifference = 0;
+
+      console.log(currentFriend.name);
+
+      for (let j = 0; j < currentFriend.scores.length; j++) {
+        const currentFriendScore = currentFriend.scores[j];
+        const currentUserScore = userScores[j];
+
+        totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+      }
+
+      if (totalDifference <= bestMatch.friendDifference) {
+        bestMatch.name = currentFriend.name;
+        bestMatch.photo = currentFriend.photo;
+        bestMatch.friendDifference = totalDifference;
+      }
+    }
+
+   
+    friends.push(userData);
+
+    res.json(bestMatch);
+  });
+};
+
