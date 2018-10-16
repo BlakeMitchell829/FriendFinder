a3eec59fe6bcc9ab42aaaecff0ab65fd78967d47
Blake Mitchell <38139493+BlakeMitchell829@users.noreply.github.com>
2018-10-15 20:08:45 -0500

    test

--- server.js
+++ server.js
@@ -4,19 +4,6 @@ const bodyParser = require("body-parser");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
-<<<<<<< HEAD
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-require("../app/routing/apiRoutes")(app);
-require("../app/routing/htmlRoutes")(app);
-
-app.listen(PORT, () => {
-  console.log("Server listening on PORT: " + PORT);
-});
-
-=======
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -27,4 +14,4 @@ require("./app/routing/htmlRoutes")(app);
 app.listen(PORT, () => {
   console.log("Server listening on PORT: " + PORT);
 });
->>>>>>> d1e31451c9feb6e70241a5b977114c6069a7ac3a
+
