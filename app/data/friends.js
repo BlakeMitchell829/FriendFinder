c852bb1d7a4fa2a806c862ba2153c4cfb8ad647d
Blake Mitchell <38139493+BlakeMitchell829@users.noreply.github.com>
2018-10-15 19:35:54 -0500

    Update friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,163 +1,54 @@
 const friends = [
-    {
-        "name":"Paige",
-        "photo":"https://via.placeholder.com/200x200.jpg",
-        "scores":[
-           5,
-           1,
-           4,
-           4,
-           5,
-           1,
-           2,
-           5,
-           4,
-           1
-            ]
-        },
-    {
-    "name":"Cindy",
-    "photo":"https://via.placeholder.com/200x200.jpg",
-    "scores":[
-       5,
-       1,
-       4,
-       4,
-       5,
-       1,
-       2,
-       5,
-       4,
-       1
-        ]
+  {
+    name: "Paige",
+    photo: "https://via.placeholder.com/200x200.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   },
   {
-    "name":"Theresa",
-    "photo":"https://via.placeholder.com/200x200.jpg",
-    "scores":[
-       9,
-       1,
-       4,
-       4,
-       5,
-       1,
-       2,
-       5,
-       4,
-       1
-        ]
+    name: "Cindy",
+    photo: "https://via.placeholder.com/200x200.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   },
   {
-    "name":"Anthony",
-    "photo":"https://via.placeholder.com/200x200.jpg",
-    "scores":[
-       5,
-       1,
-       4,
-       3,
-       2,
-       6,
-       9,
-       8,
-       9,
-       9
-        ]
+    name: "Theresa",
+    photo: "https://via.placeholder.com/200x200.jpg",
+    scores: [9, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   },
   {
-    "name":"John",
-    "photo":"https://via.placeholder.com/200x200.jpg",
-    "scores":[
-       5,
-       1,
-       4,
-       3,
-       2,
-       6,
-       9,
-       8,
-       9,
-       9
-        ]
+    name: "Anthony",
+    photo: "https://via.placeholder.com/200x200.jpg",
+    scores: [5, 1, 4, 3, 2, 6, 9, 8, 9, 9]
   },
   {
-    "name":"Kaleb",
-    "photo":"https://via.placeholder.com/200x200.jpg",
-    "scores":[
-       5,
-       1,
-       4,
-       3,
-       2,
-       6,
-       9,
-       8,
-       9,
-       9
-        ]
+    name: "John",
+    photo: "https://via.placeholder.com/200x200.jpg",
+    scores: [5, 1, 4, 3, 2, 6, 9, 8, 9, 9]
   },
   {
-    "name":"Alyssa",
-    "photo":"https://via.placeholder.com/200x200.jpg",
-    "scores":[
-       5,
-       1,
-       4,
-       3,
-       2,
-       6,
-       9,
-       8,
-       9,
-       9
-        ]
+    name: "Kaleb",
+    photo: "https://via.placeholder.com/200x200.jpg",
+    scores: [5, 1, 4, 3, 2, 6, 9, 8, 9, 9]
   },
   {
-    "name":"Jenna",
-    "photo":"https://via.placeholder.com/200x200.jpg",
-    "scores":[
-       5,
-       1,
-       4,
-       3,
-       2,
-       6,
-       9,
-       8,
-       9,
-       9
-        ]
+    name: "Alyssa",
+    photo: "https://via.placeholder.com/200x200.jpg",
+    scores: [5, 1, 4, 3, 2, 6, 9, 8, 9, 9]
   },
   {
-    "name":"James",
-    "photo":"https://via.placeholder.com/200x200.jpg",
-    "scores":[
-       5,
-       1,
-       4,
-       3,
-       2,
-       6,
-       9,
-       8,
-       9,
-       9
-        ]
+    name: "Jenna",
+    photo: "https://via.placeholder.com/200x200.jpg",
+    scores: [5, 1, 4, 3, 2, 6, 9, 8, 9, 9]
   },
   {
-    "name":"David",
-    "photo":"https://via.placeholder.com/200x200.jpg",
-    "scores":[
-       5,
-       1,
-       4,
-       3,
-       2,
-       6,
-       9,
-       8,
-       9,
-       9
-        ]
+    name: "James",
+    photo: "https://via.placeholder.com/200x200.jpg",
+    scores: [5, 1, 4, 3, 2, 6, 9, 8, 9, 9]
+  },
+  {
+    name: "David",
+    photo: "https://via.placeholder.com/200x200.jpg",
+    scores: [5, 1, 4, 3, 2, 6, 9, 8, 9, 9]
   }
-  ]
-  module.exports = friends;
+];
+module.exports = friends;
+
