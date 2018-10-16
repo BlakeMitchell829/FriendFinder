f8f38d0c3b96e49e71d398a3b3510a98fa130b93
Blake Mitchell <38139493+BlakeMitchell829@users.noreply.github.com>
2018-10-15 20:04:11 -0500

    express

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-const PORT = process.env.PORT || 8100;
+const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
